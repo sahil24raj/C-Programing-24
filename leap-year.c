ab2511b488a1928360af4a0d5c78e4355c255cc2
Sahil Raj <sahil24raj24@gmail.com>
2025-08-30 11:53:26 +0530

    Add files via upload

---
+++ leap-year.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int year=2024;
+    if((year%400==0) || (year%4==0 && year%100!=0))
+        printf("Leap Year");
+    else
+        printf("Not Leap Year");
+    return 0;
+}
+
